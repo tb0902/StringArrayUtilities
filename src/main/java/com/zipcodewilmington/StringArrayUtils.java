65d890863fcedbb531dfd432f1932e06c74da4f3
tb0902 <tbruton417@gmail.com>
2024-03-10 11:57:57 -0400

    Passed the first half of tests

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -11,7 +11,7 @@ public class StringArrayUtils {
     public static String getFirstElement(String[] array) {
         return array[0];
     }
-
+//passes
     /**
      * @param array array of String objects
      * @return second element in specified array
@@ -19,40 +19,50 @@ public class StringArrayUtils {
     public static String getSecondElement(String[] array) {
         return array[1];
     }
-
+//passes
     /**
      * @param array array of String objects
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        return null;
+        return array[array.length-1];
     }
-
+//passes
     /**
      * @param array array of String objects
      * @return second to last element in specified array
      */ // TODO
     public static String getSecondToLastElement(String[] array) {
-        return null;
+        return array[array.length-2];
     }
-
+//passes
     /**
      * @param array array of String objects
      * @param value value to check array for
      * @return true if the array contains the specified `value`
      */ // TODO
     public static boolean contains(String[] array, String value) {
+        for (int i = 0; i < array.length; i++){
+            if (value == array[i]);
+            return true;
+        }
         return false;
     }
-
+// passes
     /**
      * @param array of String objects
      * @return an array with identical contents in reverse order
      */ // TODO
     public static String[] reverse(String[] array) {
-        return null;
+        String[] reversedArray = new String[array.length]; // creates new array at previous array length
+        int temp = array.length; // creates counter that is just like i, but at the end of the array
+       for (int i = 0; i < array.length; i++) { // iterates through array
+           reversedArray[temp-1] = array[i]; // temp is at the end, i is at the beginning
+           temp = temp-1; // makes the two switch
+       }
+        return reversedArray;
     }
-
+// passes
     /**
      * @param array array of String objects
      * @return true if the order of the array is the same backwards and forwards
